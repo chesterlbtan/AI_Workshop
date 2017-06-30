fd99bc1f195bc2aa9b9aec5cca017bc0f46a5884
chesterlbtan <chesterlbtan@gmail.com>
2017-06-30 22:37:34 +0800

    Added validation at the end of the training
    temporarily decrease the number of training loop

--- mlp_test_iris.m
+++ mlp_test_iris.m
@@ -5,7 +5,7 @@ clc
 myMLP = create_mlp(4,3);
 errorList = [];
 
-for s = 1:75
+for s = 1:50
     fileID = fopen('data\iris.csv');
     tline = fgetl(fileID);
     while tline > 0
@@ -30,5 +30,34 @@ for s = 1:75
     fclose(fileID);
 end
 
+
+classA = zeros(1,3);
+classB = zeros(1,3);
+classC = zeros(1,3);
+
+fileID = fopen('data\iris.csv');
+tline = fgetl(fileID);
+while tline > 0
+    data = strsplit(tline, ',');
+    input = [str2double(data(1)) str2double(data(2)) str2double(data(3)) str2double(data(4))];
+    
+    [outActual, hiddenAct, inputWithW] = getoutput(myMLP, input);
+    
+    % get the max
+    index = find(outActual==max(outActual),1);
+    
+    adder = zeros(1,3);
+    adder(index) = adder(index) + 1;
+    if strcmp(data(5), 'Iris-setosa')
+        classA = classA + adder;
+    elseif strcmp(data(5),'Iris-versicolor')
+        classB = classB + adder;
+    else
+        classC = classC + adder;
+    end
+    
+    tline = fgetl(fileID);
+end
+fclose(fileID);
 
 
